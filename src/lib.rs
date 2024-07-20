e3dbf4d6c53e1c1d62267274bc5cb42102f54c3c
William Tremblay <tremwil@gmail.com>
2024-07-20 15:28:53 -0400

    add per-game features

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+#[cfg(any(
+    all(feature = "er", feature = "ds3"),
+    all(feature = "ds3", feature = "ac6"),
+    all(feature = "ac6", feature = "er")
+))]
+compile_error!("Only one of the target game features (ds3, er, ac6) may be enabled");
+
 mod celua;
